d9eae8b8b750279c0ae7f0b418a9486fb02ac9bf
Сергей Елисеев <gorguls@mail.ru>
2021-10-27 16:51:21 +0300

    Авторизация из документации

---
+++ src/main/java/by/bw/security/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package by.bw.security.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class HelloController {
+//    @GetMapping("/")
+//    public String hello(Map<String, Object> model) {
+//        return "hello";
+//    }
+}
